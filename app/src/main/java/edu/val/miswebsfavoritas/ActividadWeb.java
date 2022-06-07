014b94862f44b0adbc8c2a207b0c87e63a6445c4
valexx55 <valexx55@gmail.com>
2022-06-07 21:55:12 +0200

    WEB VIEW remota

--- app/src/main/java/edu/val/miswebsfavoritas/ActividadWeb.java
+++ app/src/main/java/edu/val/miswebsfavoritas/ActividadWeb.java
@@ -9,6 +9,7 @@ public class ActividadWeb extends AppCompatActivity {
 
     private WebView webView;//vista para representar una web HTML
     private static final String WEB_LOCAL = "file:///android_asset/aviso.html";
+    private static final String WEB_REMOTA = "https://www.google.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +17,10 @@ public class ActividadWeb extends AppCompatActivity {
         setContentView(R.layout.activity_actividad_web);
         this.webView = findViewById(R.id.webview);
         //CARGAR la página local aviso que hay en assets
-        this.webView.loadUrl(WEB_LOCAL);
+        //this.webView.loadUrl(WEB_LOCAL);
+        //CARGAR UNA WEB REMOTA
+        this.webView.loadUrl(WEB_REMOTA);//para poder cargar una web remota, necesito darle el permiso de conexión a internet
+
 
     }
 }
